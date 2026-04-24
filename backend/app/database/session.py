463475aae580c70afd931ac93e9ea01053acc8f6
Adnan Khan <sr863269@gmail.com>
2026-04-24 09:04:55 +0530

    Fix DB + main config

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -2,11 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-# 📁 Create DB file path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DB_PATH = os.path.join(BASE_DIR, "test.db")
 
-# 🗄️ SQLite DB
 DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
