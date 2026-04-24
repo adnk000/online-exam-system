a2a2d6638931da2df5f195061a24a592014ebf70
Adnan Khan <sr863269@gmail.com>
2026-04-24 08:55:05 +0530

    Fix DB for deployment

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,9 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:Adnan%40123@localhost/exam_db"
+# 📁 Create DB file path
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DB_PATH = os.path.join(BASE_DIR, "test.db")
 
-engine = create_engine(DATABASE_URL)
+# 🗄️ SQLite DB
+DATABASE_URL = f"sqlite:///{DB_PATH}"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(bind=engine)
 
