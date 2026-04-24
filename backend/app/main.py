0e09b250d6b67bf1cb3a19cab57a5f5b8fbea53f
Adnan Khan <sr863269@gmail.com>
2026-04-24 11:04:33 +0530

    Add auto seed for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,3 +36,41 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth_router)
 app.include_router(question.router)
 app.include_router(admin.router)
+
+
+from app.database.session import SessionLocal
+from app.models.user import User
+from app.models.question import Question
+
+@app.on_event("startup")
+def seed_data():
+    db = SessionLocal()
+
+    # 👨‍🎓 Users
+    if not db.query(User).first():
+        db.add(User(email="student1@test.com", password="1234"))
+        db.add(User(email="student2@test.com", password="1234"))
+        db.add(User(email="admin@test.com", password="admin123"))
+
+    # 🧠 Questions
+    if not db.query(Question).first():
+        db.add(Question(
+            question_text="What is 2 + 2?",
+            option_a="3",
+            option_b="4",
+            option_c="5",
+            option_d="6",
+            correct_option="4"
+        ))
+
+        db.add(Question(
+            question_text="Capital of India?",
+            option_a="Mumbai",
+            option_b="Delhi",
+            option_c="Chennai",
+            option_d="Kolkata",
+            correct_option="Delhi"
+        ))
+
+    db.commit()
+    db.close()
